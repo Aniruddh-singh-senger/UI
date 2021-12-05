e84100b3f8cb51167174615666f6efcc521a6cdd
Aniruddh-singh-senger <89852865+Aniruddh-singh-senger@users.noreply.github.com>
2021-12-05 20:02:25 +0530

    Update ProductFile.jsx

--- src/ProductFile.jsx
+++ src/ProductFile.jsx
@@ -1,73 +1,22 @@
 import React from 'react';
 
-const ProductFile = () => {
+const ProductFile = (props) => {
+
+    let { product_name, brand_name, discription, price, date, image, state } = props
     return (
         <>
-            <div>
-                <p>Product Name</p>
-                <hr style={{ background: 'white', }} />
-                <div style={{ background: 'black', padding: '20px', borderRadius: '20px', display: 'flex' }}>
-                    <div className="col-md-6 col-lg-4 py-3">
-                        <div className="card-blog" style={{ background: 'grey' }}>
-                            <div >
-                                <div className="avatar">
-                                    <img src="../assets/img/person/person_1.jpg" alt="" />
-                                </div>
-                                <div className="entry-footer">
-                                    <h5 >Product Name</h5>
-                                    <h6>Brand Name</h6>
-                                    <h6>$ 29.99</h6>
-                                </div>
-                            </div>
-                            <div className="footer">
-                                <div style={{ display: 'flex', justifyContent: 'space-between' }}> <p >Location</p>
-                                    <p >Date:10:12:2020</p></div>
-                                Descrpition of the Product/item
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-6 col-lg-4 py-3">
-                        <div className="card-blog" style={{ background: 'grey' }}>
-                            <div >
-                                <div className="avatar">
-                                    <img src="../assets/img/person/person_1.jpg" alt="" />
-                                </div>
-                                <div className="entry-footer">
-                                    <h5 >Product Name</h5>
-                                    <h6>Brand Name</h6>
-                                    <h6>$ 29.99</h6>
-                                </div>
-                            </div>
-                            <div className="footer">
-                                <div style={{ display: 'flex', justifyContent: 'space-between' }}> <p >Location</p>
-                                    <p >Date:10:12:2020</p></div>
-                                Descrpition of the Product/item
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-6 col-lg-4 py-3">
-                        <div className="card-blog" style={{ background: 'grey' }}>
-                            <div >
-                                <div className="avatar">
-                                    <img src="../assets/img/person/person_1.jpg" alt="" />
-                                </div>
-                                <div className="entry-footer">
-                                    <h5 >Product Name</h5>
-                                    <h6>Brand Name</h6>
-                                    <h6>$ 29.99</h6>
-                                </div>
-                            </div>
-                            <div className="footer">
-                                <div style={{ display: 'flex', justifyContent: 'space-between' }}> <p >Location</p>
-                                    <p >Date:10:12:2020</p></div>
-                                Descrpition of the Product/item
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div style={{ background: 'grey', padding: '5px', display: 'grid', gridTemplateColumns: "1fr 1fr", gridTemplateRows: '1fr 1fr 1fr 1fr 1fr', gridGap: '10px', height: '80%' }} >
+                <img src={image} style={{ width: '90%', gridRow: '1/3' }} alt="..." />
+                <h5>{product_name}</h5>
+                <p> {brand_name} </p>
+                <p style={{ gridColumn: '2/3' }}>${price}</p>
+                <p>{state} </p>
+                <p>Date: {date}</p>
+                <p style={{ gridColumn: '1/-1' }} >{discription}</p>
             </div>
+
         </>
     )
 }
 
-export default ProductFile
+export default ProductFile;
